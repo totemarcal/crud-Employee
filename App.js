93aad5acad2da64fc47f5e4b7c602ce2bdc40500
totemarcal <totemarcal@gmail.com>
2021-01-21 20:09:15 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import ListEmployeeModal from './src/ListEmployeeModal'
+
+export default function App() {
+  return (
+    <View >
+      <ListEmployeeModal />      
+    </View>
+  );
+}
